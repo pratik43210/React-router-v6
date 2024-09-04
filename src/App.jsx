df81755249c63337ba7340423da057e63b9d8654
Pratik Raja <pratikraja2644@gmail.com>
2024-09-04 15:16:22 +0530

    links and basic route implementation

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,35 @@
-import './App.css'
-
+import {Link, Route, Routes} from 'react-router-dom'
+import Home from "./pages/Home.jsx"
+import About from "./pages/About.jsx"
+import BookList from "./pages/Booklist.jsx"
+import Contact from "./pages/Contact.jsx"
 function App() {
 
   return (
     <>
-      Hello
+      <ul>
+        <li>
+          <Link to="/">Home</Link> 
+        </li>
+        <li>
+          <Link to="/about">About</Link> 
+        </li>
+        <li>
+          <Link to="/booklist">BookList</Link> 
+        </li>
+        <li>
+          <Link to="/contact">Contact</Link> 
+        </li>
+      </ul>
+
+      <br></br>
+
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/about" element={<About/>}/>
+        <Route path="/booklist" element={<BookList/>}/>
+        <Route path="/contact" element={<Contact/>}/>
+      </Routes>
     </>
   )
 }
